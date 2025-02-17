27b9fd3a01aeac20271b5ba2845d0314e21eff94
NabilRaiyan <rraiyan77@gmail.com>
2025-02-17 22:21:06 +0600

    creating service section

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,17 +72,9 @@ export default function RootLayout({
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
+                  Copyright 2025{" "}
+                  <span className="font-space text-cyan-700">ByteMorphIT</span>
                 </p>
-                <ThemeSwitcher />
               </footer>
             </div>
           </main>
