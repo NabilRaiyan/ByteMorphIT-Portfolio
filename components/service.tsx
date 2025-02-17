57f373a7281d389e8e976ad0df10216a816ac54c
NabilRaiyan <rraiyan77@gmail.com>
2025-02-17 22:33:45 +0600

    creating service section

--- components/service.tsx
+++ components/service.tsx
@@ -9,7 +9,17 @@ export default function Service(){
 
             <hr className="w-32 h-1 mx-auto bg-gradient-to-r from-amber-400 via-amber-500 to-orange-600 border-0 rounded-sm dark:bg-gray-700"/>
 
-            <div className="flex flex-row gap-8 mt-16 overflow-hidden">
+            <div className="flex flex-row gap-8 mt-16 overflow-hidden flex-wrap p-10 items-center justify-center">
+            <ServiceCard />
+            <ServiceCard />
+            <ServiceCard />
+            <ServiceCard />
+
+            <ServiceCard />
+            <ServiceCard />
+            <ServiceCard />
+            <ServiceCard />
+
             <ServiceCard />
             <ServiceCard />
             <ServiceCard />
