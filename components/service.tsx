7864a14b43150073dde3c7675fb0fcd98036be4c
NabilRaiyan <rraiyan77@gmail.com>
2025-02-17 22:28:38 +0600

    creating service section

--- components/service.tsx
+++ components/service.tsx
@@ -1,3 +1,4 @@
+import ServiceCard from "./ui/service-card";
 
 
 export default function Service(){
@@ -8,6 +9,13 @@ export default function Service(){
 
             <hr className="w-32 h-1 mx-auto bg-gradient-to-r from-amber-400 via-amber-500 to-orange-600 border-0 rounded-sm dark:bg-gray-700"/>
 
+            <div className="flex flex-row gap-8 mt-16 overflow-hidden">
+            <ServiceCard />
+            <ServiceCard />
+            <ServiceCard />
+            <ServiceCard />
+           
+            </div>
         </section>
     )
 }
---
+++ components/ui/service-card.tsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+const ServiceCard = () => {
+  return (
+    <div className='bg-gradient-to-r from-amber-400 to-orange-700 text-white p-6 rounded'>
+      <h1>Frontend</h1>
+      <p>We provide frontend solution</p>
+    </div>
+  )
+}
+
+export default ServiceCard
