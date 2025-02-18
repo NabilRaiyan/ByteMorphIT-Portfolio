94569541323bbcc8f2733d9f3dda3f09ebd1f87b
NabilRaiyan <rraiyan77@gmail.com>
2025-02-18 23:07:00 +0600

    creating service section

--- components/service.tsx
+++ components/service.tsx
@@ -19,6 +19,7 @@ export default function Service(){
             <ServiceCard />
             <ServiceCard />
             <ServiceCard />
+            
             <ServiceCard />
             <ServiceCard />
             <ServiceCard />
