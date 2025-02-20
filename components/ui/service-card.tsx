371ed1e6982ad7fe4460335f521744d5031fd273
NabilRaiyan <rraiyan77@gmail.com>
2025-02-20 21:31:22 +0600

    creating service section

--- components/ui/service-card.tsx
+++ components/ui/service-card.tsx
@@ -2,10 +2,16 @@
 
 import React from 'react'
 
-const ServiceCard = () => {
+interface cardProps {
+  title: string,
+  description: string,
+  icon: string
+}
+
+const ServiceCard = ({title, description, icon}:cardProps) => {
   return (
     <div className='bg-gradient-to-r from-amber-400 to-orange-700 text-white p-6 rounded'>
-      <h1>Frontend</h1>
+      <h1>{title}</h1>
       <p>We provide frontend solution</p>
     </div>
   )
